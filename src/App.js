460e33638b8a67347862ab31091e67a867293afd
ersaurabhtiwari <ersaur200185@gmail.com>
2022-01-25 01:49:42 +0530

    my first commit reactproject

---
+++ src/App.js
@@ -0,0 +1,58 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Textarea from './Components/Textarea';
+import About from './Components/About';
+import React,{useState} from 'react';
+import Alert from './Components/Alert';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert=(message,type)=>{
+     setAlert({
+       msg:message,
+       type:type
+     })
+     setTimeout(() => {
+       setAlert(null);
+     }, 1000);
+  }
+  const toogleMode=()=>{
+       if(mode==="light"){
+         setMode("dark");
+         document.body.style.backgroundColor='gray';
+         showAlert("Dark mode has been Enabled","success");
+         document.title="textUtils-dark mode";
+       }
+       else{
+         setMode("light");
+         document.body.style.backgroundColor='white';
+         showAlert("Light mode has been Enabled","success");
+         document.title="textUtils-light mode";
+       }
+  }
+  return (
+    <>
+    <Router>
+        <Navbar ram="TextUtils" mode={mode} shyam="Home" kam="About Us" toogleMode={toogleMode}/>
+          <Alert alert={alert}/>
+        <div className="container my-5">
+           <Routes>
+                <Route exact path="/about" element={<About/>} />
+                <Route  exact path="/" element= {<Textarea Textarea="Write text here in below" showAlert={showAlert} mode={mode}/>} />                 
+           </Routes> 
+        </div>    
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Textarea.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+
+export default function Textarea(props) {
+  const handleUpCase = () => {
+    // console.log("clicked"+Text);
+    let newText = Text.toUpperCase();
+    setText(newText);
+    props.showAlert("Upper case has been enabled","success");
+  };
+  const handleLoCase = () => {
+    // console.log("clicked"+Text);
+    let newText = Text.toLowerCase();
+    setText(newText);
+    props.showAlert("Lower case has been enabled","success");
+  };
+  const handleClear = () => {
+    // console.log("clicked"+Text);
+    let newText = "";
+    setText(newText);
+    props.showAlert("Cleared text","success");
+  };
+  const handleText = (event) => {
+    // console.log("message in Uppercase");
+    setText(event.target.value);
+  };
+  const handleCopy=()=>{
+    var text =document.getElementById("floatingTextarea2");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("copied to clipboard","success");
+  };
+  const [Text, setText] = useState("");
+  // Text="Newtext"; This is wrong way
+  // setText("NewText");this is right way
+  return (
+    <>
+      <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+        <div className="form-floating">
+          <h1>{props.Textarea}</h1>
+          <textarea
+            className="form-control"
+            value={Text}
+            onChange={handleText}
+            placeholder="Leave a comment here"
+            id="floatingTextarea2"
+            style={{ height: "300px",backgroundColor: props.mode==='dark'?'gray':'white' ,color: props.mode==='dark'?'white':'black'}}
+            defaultValue={""}
+          />
+          <button
+            type="button"
+            className="btn btn-primary my-3"
+            onClick={handleUpCase}
+          >
+            Convert to Uppercase
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary mx-3"
+            onClick={handleLoCase}
+          >
+            Convert to Lowercase
+          </button>
+          <button
+            type="button"
+            className="btn btn-danger mx-2"
+            onClick={handleClear}
+          >
+            Clear Text
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary mx-3"
+            onClick={handleCopy}
+          >
+          Copy Text
+          </button>
+        </div>
+      </div>
+      <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h1>About above paragraphs</h1>
+        <p>
+          No.of words {Text.split(" ").length} and Character {Text.length}
+        </p>
+        <p>{0.008 * Text.split(" ").length} Minute read</p>
+        <h2>Preview</h2>
+        <p>{Text}</p>
+      </div>
+    </>
+  );
+}
