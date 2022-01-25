91abb9cdde52e520f8ea971aaabeb7f573af4364
ersaurabhtiwari <ersaur200185@gmail.com>
2022-01-25 15:17:45 +0530

    Updates

---
+++ precache-manifest.4fd4b0878e288543123f55135862eeba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afce1d5aaee22bd6812383c160a772dd",
+    "url": "/TextUtil/index.html"
+  },
+  {
+    "revision": "e836974de5cdec668d17",
+    "url": "/TextUtil/static/css/main.7834b833.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/TextUtil/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/TextUtil/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "e836974de5cdec668d17",
+    "url": "/TextUtil/static/js/main.060243f8.chunk.js"
+  },
+  {
+    "revision": "e256bcc56cc9aee0c81c",
+    "url": "/TextUtil/static/js/runtime~main.53250eb2.js"
+  }
+]);
