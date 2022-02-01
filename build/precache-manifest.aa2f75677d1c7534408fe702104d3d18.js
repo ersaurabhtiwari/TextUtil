e8173a37d4ed2abdfba593a2ef30f0c8e36d1d91
SAURABH TIWARI <97665532+ersaurabhtiwari@users.noreply.github.com>
2022-02-01 13:06:31 +0530

    Add files via upload

---
+++ build/precache-manifest.aa2f75677d1c7534408fe702104d3d18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f571ea82c8567650729e247ba812cce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0909b04aa202cd621289",
+    "url": "/static/css/main.7834b833.chunk.css"
+  },
+  {
+    "revision": "fbe61067847c922b3dc8",
+    "url": "/static/js/2.0af97a9d.chunk.js"
+  },
+  {
+    "revision": "1611c050afd48d2ff311",
+    "url": "/static/js/3.26aa4bf2.chunk.js"
+  },
+  {
+    "revision": "0909b04aa202cd621289",
+    "url": "/static/js/main.4344f88d.chunk.js"
+  },
+  {
+    "revision": "193e70c58e5af559b55c",
+    "url": "/static/js/runtime~main.88519e64.js"
+  }
+]);
